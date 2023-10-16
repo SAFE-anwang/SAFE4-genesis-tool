0e3d59e4fa817896dbc26483794aab36774da658
lemengbin <lemengbin@163.com>
2023-10-16 20:59:12 +0800

    remove includePath for solc

--- utils/solc.go
+++ utils/solc.go
@@ -9,10 +9,9 @@ func Compile(workPath string, fileName string) {
 	solcPath := workPath + "solc.exe"
 	filePath := workPath + "SAFE4-system-contract" + string(filepath.Separator)
 	basePath := filePath
-	includePath := filePath + ".deps" + string(filepath.Separator) + "npm"
 	dstPath := workPath + "temp"
 	//cmd := exec.Command(solcPath, "--base-path", basePath, "--include-path", includePath, "--optimize-runs", "200", "--bin-runtime", "--abi", "--storage-layout", "-o", dstPath, "--overwrite", filePath + fileName)
-	cmd := exec.Command(solcPath, "--base-path", basePath, "--include-path", includePath, "--optimize-runs", "200", "--bin-runtime", "-o", dstPath, "--overwrite", filePath + fileName)
+	cmd := exec.Command(solcPath, "--base-path", basePath, "--optimize-runs", "200", "--bin-runtime", "-o", dstPath, "--overwrite", filePath + fileName)
 	_, err := cmd.Output()
 	if err != nil {
 		panic(err)
@@ -23,10 +22,9 @@ func GetABI(workPath string, fileName string) {
 	solcPath := workPath + "solc.exe"
 	filePath := workPath + "SAFE4-system-contract" + string(filepath.Separator)
 	basePath := filePath
-	includePath := filePath + ".deps" + string(filepath.Separator) + "npm"
 	dstPath := workPath + "temp"
 	//cmd := exec.Command(solcPath, "--base-path", basePath, "--include-path", includePath, "--optimize-runs", "200", "--bin-runtime", "--abi", "--storage-layout", "-o", dstPath, "--overwrite", filePath + fileName)
-	cmd := exec.Command(solcPath, "--base-path", basePath, "--include-path", includePath, "--optimize-runs", "200", "--abi", "-o", dstPath, "--overwrite", filePath + fileName)
+	cmd := exec.Command(solcPath, "--base-path", basePath, "--optimize-runs", "200", "--abi", "-o", dstPath, "--overwrite", filePath + fileName)
 	_, err := cmd.Output()
 	if err != nil {
 		panic(err)
