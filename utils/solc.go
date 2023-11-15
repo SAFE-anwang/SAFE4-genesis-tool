fe602663e077753d62dc8e236be4ee35d68f7018
lemengbin <lemengbin@163.com>
2023-11-15 15:37:43 +0800

    async SAFE4-system-contract

--- utils/solc.go
+++ utils/solc.go
@@ -10,8 +10,9 @@ func Compile(workPath string, fileName string) {
 	filePath := workPath + "SAFE4-system-contract" + string(filepath.Separator)
 	basePath := filePath
 	dstPath := workPath + "temp"
-	//cmd := exec.Command(solcPath, "--base-path", basePath, "--include-path", includePath, "--optimize-runs", "200", "--bin-runtime", "--abi", "--storage-layout", "-o", dstPath, "--overwrite", filePath + fileName)
-	cmd := exec.Command(solcPath, "--base-path", basePath, "--optimize-runs", "200", "--bin-runtime", "-o", dstPath, "--overwrite", filePath + fileName)
+	openzeppelin_upgradeable_alias := "openzeppelin-contracts-upgradeable/=3rd/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/"
+	openzeppelin_alias := "openzeppelin-contracts/=3rd/OpenZeppelin/openzeppelin-contracts/contracts/"
+	cmd := exec.Command(solcPath, "--base-path", basePath, "--optimize-runs", "200", "--bin-runtime", "-o", dstPath, openzeppelin_upgradeable_alias, openzeppelin_alias, "--overwrite", filePath + fileName)
 	_, err := cmd.Output()
 	if err != nil {
 		panic(err)
@@ -23,8 +24,9 @@ func GetABI(workPath string, fileName string) {
 	filePath := workPath + "SAFE4-system-contract" + string(filepath.Separator)
 	basePath := filePath
 	dstPath := workPath + "temp"
-	//cmd := exec.Command(solcPath, "--base-path", basePath, "--include-path", includePath, "--optimize-runs", "200", "--bin-runtime", "--abi", "--storage-layout", "-o", dstPath, "--overwrite", filePath + fileName)
-	cmd := exec.Command(solcPath, "--base-path", basePath, "--optimize-runs", "200", "--abi", "-o", dstPath, "--overwrite", filePath + fileName)
+	openzeppelin_upgradeable_alias := "openzeppelin-contracts-upgradeable/=3rd/OpenZeppelin/openzeppelin-contracts-upgradeable/contracts/"
+	openzeppelin_alias := "openzeppelin-contracts/=3rd/OpenZeppelin/openzeppelin-contracts/contracts/"
+	cmd := exec.Command(solcPath, "--base-path", basePath, "--optimize-runs", "200", "--abi", "-o", dstPath, openzeppelin_upgradeable_alias, openzeppelin_alias, "--overwrite", filePath + fileName)
 	_, err := cmd.Output()
 	if err != nil {
 		panic(err)
