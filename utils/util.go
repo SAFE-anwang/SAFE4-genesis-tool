240e38b1362a7051c433fc231d72256e09e5bf2d
lemengbin <lemengbin@163.com>
2023-12-21 22:09:47 +0800

    init Contract Owner

--- utils/util.go
+++ utils/util.go
@@ -10,3 +10,12 @@ func GetDataDir() string {
     }
     return "data"
 }
+
+func GetOwnerAddr() string {
+    for _ ,v := range os.Args {
+        if v == "-test" {
+            return "80d8b8f308770ce14252173abb00075cc9082d03"
+        }
+    }
+    return "0xac110c0f70867f77d9d230e377043f52480a0b7d"
+}
